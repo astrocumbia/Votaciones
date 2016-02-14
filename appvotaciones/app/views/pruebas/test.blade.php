cd5f41afaa06fd2f5a8f90693577438f44a2c6ed
backtrack <cruzmendezalan@gmail.com>
2016-02-14 12:07:56 -0600

    Se agrego un contenedor faltante

--- appvotaciones/app/views/pruebas/test.blade.php
+++ appvotaciones/app/views/pruebas/test.blade.php
@@ -1,53 +1,10 @@
 @extends('layouts.layoutbase')
 @section('body')
-	<div class="container">
-	<div class="text-center">
-		<h3>Cabecera</h3>
-	</div>
-		<div class="row">
-			<p>
-				{{ $perros }}
-			</p>
-		</div>
-		<div class="row">
-			<table class="table table-hover">
-				<thead>
-					<tr>
-					  @foreach( $funcionarios[0]->getAttributes() as $etiqueta => $valor)
-				        <td>{{$etiqueta}}</td>
-				      @endforeach
-					</tr>
-				</thead>
-				<tbody>
-					
-				</tbody>
-			</table>
-		</div>
-		<div class="row">
-			<div class="col-md-6 bg-primary">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque praesentium exercitationem adipisci blanditiis cupiditate qui vero eligendi, repudiandae id reiciendis tempora? Excepturi molestias dicta nobis cum perferendis ad natus? Doloribus.
-			</div>
-			<div class="col-md-2 bg-success">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint dolores, quae et porro quidem vero quaerat quasi eligendi fuga qui tempora facere minima consequatur similique alias tenetur vitae quam dolorem?
-			</div>
-			<div class="col-md-2 bg-info">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo necessitatibus dolor aliquam, perspiciatis enim eaque hic consequatur harum fugit, veniam esse beatae provident unde minus libero ut accusantium repudiandae labore.
-			</div>
-			<div class="col-md-1 bg-danger">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum cum, commodi consequatur fuga voluptatem molestiae ea, expedita amet explicabo beatae animi nobis fugiat libero itaque, ducimus incidunt quibusdam ratione nisi.
-			</div>
-			<div class="col-md-1 bg-warning">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto amet, accusantium voluptas id perferendis ullam tempore impedit minus facilis numquam quod pariatur dolores vitae hic quia! Amet maiores nemo reprehenderit.
-			</div>
-		</div>
-	</div>
+	Codigo HTML
 @endsection
 
 @section('javascript')
-	{{-- Aqui colocan el link a sus scripts
-		o si lo van a crear ustedes pueden hacerlo normalmente con <script> </script>
-		**Recuerden que el js debe estar dentro de la carpeta js en public
-	 --}}
+	
 	<script>
 		$( document ).ready(function() {
 		    console.log( "ready!" );
