0d85ae5036acb7476d6a62b5f50b9db42743768c
OmrSC <tirengarfio@gmail.com>
2016-02-15 04:50:53 -0600

    funcion getmac()

--- appvotaciones/app/views/pruebas/obtenermac.php
+++ appvotaciones/app/views/pruebas/obtenermac.php
@@ -1,61 +1,11 @@
 <!DOCTYPE  HTML  PUBLIC  "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
   <head>
-        <title></title>
-        <meta name="GENERATOR" content="Microsoft Visual Studio .NET 7.1">
-        <meta name="vs_targetSchema" content="http://schemas.microsoft.com/intellisense/ie5">
-        <script id="clientEventHandlersJS" language="javascript">
-
-function Button1_onclick() {
-  var locator = new ActiveXObject("WbemScripting.SWbemLocator");
-  var service = locator.ConnectServer(".");
-  var properties = service.ExecQuery("SELECT * FROM Win32_NetworkAdapterConfiguration");
-  var e = new Enumerator (properties);
-  document.write("<table border=1>");
-  dispHeading();
-  for (;!e.atEnd();e.moveNext ())
-  {
-        var p = e.item ();
-        if(p.IPFilterSecurityEnabled==false){
-          alert(p.MACAddress);
-        }
-        document.write("<tr>");
-        document.write("<td>" + p.Caption + "</td>");
-        document.write("<td>" + p.IPFilterSecurityEnabled + "</td>");
-        document.write("<td>" + p.IPPortSecurityEnabled + "</td>");
-        document.write("<td>" + p.IPXAddress + "</td>");
-        document.write("<td>" + p.IPXEnabled + "</td>");
-        document.write("<td>" + p.IPXNetworkNumber + "</td>");
-        document.write("<td>" + p.MACAddress + "</td>");
-        document.write("<td>" + p.WINSPrimaryServer + "</td>");
-        document.write("<td>" + p.WINSSecondaryServer + "</td>");
-        document.write("</tr>");
-  }
-  document.write("</table>");
-}
-
-function dispHeading()
-{
-    document.write("<thead>");
-    document.write("<td>Caption</td>");
-    document.write("<td>IPFilterSecurityEnabled</td>");
-    document.write("<td>IPPortSecurityEnabled</td>");
-    document.write("<td>IPXAddress</td>");
-    document.write("<td>IPXEnabled</td>");
-    document.write("<td>IPXNetworkNumber</td>");
-    document.write("<td>MACAddress</td>");
-    document.write("<td>WINSPrimaryServer</td>");
-    document.write("<td>WINSSecondaryServer</td>");
-    document.write("</thead>");
-}
-
-        </script>
-
       <title></title>
       <meta name="GENERATOR" content="Microsoft Visual Studio .NET 7.1">
       <meta name="vs_targetSchema" content="http://schemas.microsoft.com/intellisense/ie5">
       <script id="clientEventHandlersJS" language="javascript">
-        function Button1_onclick() {
+        function getmac() {
           var locator = new ActiveXObject("WbemScripting.SWbemLocator");
           var service = locator.ConnectServer(".");
           var properties = service.ExecQuery("SELECT * FROM Win32_NetworkAdapterConfiguration");
@@ -64,6 +14,7 @@ function dispHeading()
           {
             var p = e.item ();
               if(p.IPFilterSecurityEnabled==false){
+                alert(p.MACAddress);
                 return p.MACAddress;
               }
           }
@@ -72,35 +23,5 @@ function dispHeading()
   </head>
   <body>
 
-        <INPUT id="Button1" type="button" value="Button" name="Button1" language="javascript" onclick="return Button1_onclick()">
-  </body>
-
-<!-- <!DOCTYPE  HTML  PUBLIC  "-//W3C//DTD HTML 4.0 Transitional//EN">
-<html>
-  <head>
-        <title></title>
-        <meta name="GENERATOR" content="Microsoft Visual Studio .NET 7.1">
-        <meta name="vs_targetSchema" content="http://schemas.microsoft.com/intellisense/ie5">
-        <script id="clientEventHandlersJS" language="javascript">
-
-function getmac() {
-  var locator = new ActiveXObject("WbemScripting.SWbemLocator");
-  var service = locator.ConnectServer(".");
-  var properties = service.ExecQuery("SELECT * FROM Win32_NetworkAdapterConfiguration");
-  var e = new Enumerator (properties);
-  for (i=0;!e.atEnd();e.moveNext (),i++)
-  {
-    if (i==2) {
-      var p = e.item ();
-      alert(p.MACAddress); 
-    }
-  }
-}
-
-
-        </script>
-  </head>
-  <body>
-
         <INPUT id="Button1" type="button" value="Button" name="Button1" language="javascript" onclick="getmac()">
-  </body> -->
+  </body>
