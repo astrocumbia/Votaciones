fd933f35b5d78b18575045729a8202d3d9514485
Emmanuel Pacheco Leon <pacheco.10a@gmail.com>
2016-02-15 04:15:33 -0600

    modificacion de IFETableseeder

--- appvotaciones/app/database/seeds/IFETableSeeder.php
+++ appvotaciones/app/database/seeds/IFETableSeeder.php
@@ -12,15 +12,14 @@ class CasillaTableSeeder extends Seeder {
 
         $str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
         $cad = "";
-        for($i=0;$i<12;$i++) {
-         $cad .= substr($str,rand(0,18),1);
-         }
+       
+
 
 
         for ($i = 0; $i < 100; $i++){
         	$votante = DBIFE::create(array(
         	'nombre'=>$faker->name,
-        	'clave_elector'=>$cad,
+        	'clave_elector'=>$cad .= substr($str,rand(0,62),1);,
         	'huella'=>'123456',
         	'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
             'updated_at' => \Carbon\Carbon::now()->toDateTimeString()
